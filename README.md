bd49cda07b2062b7dbabde7b0cbdf89d24698460
Girma Nigusse <xgirma@gmail.com>
2020-01-23 20:22:45 -0500

    update test

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # nightwatch (commonjs, assert)
-[![GitHub Actions status | e2e-boilerplates/nightwatch-commonjs-assert](https://github.com/e2e-boilerplates/nightwatch-commonjs-assert/workflows/NodeCI/badge.svg)](https://github.com/e2e-boilerplates/nightwatch-commonjs-assert/actions?workflow=NodeCI)
+[![GitHub Actions status | e2e-boilerplates/nightwatch-commonjs-assert](https://github.com/e2e-boilerplates/nightwatch-commonjs-assert/workflows/nightwatch-commonjs-assert/badge.svg)](https://github.com/e2e-boilerplates/nightwatch-commonjs-assert/actions?workflow=nightwatch-commonjs-assert)
 
 nightwatch end-to-end test automation boilerplate project. Using commonjs and assert.
 
--- spec/search.spec.js
+++ spec/search.spec.js
@@ -1,31 +1,16 @@
 module.exports = {
   before: browser => {
-    browser.url("https://www.google.com");
+    browser.url("https://e2e-boilerplates.github.io/sandbox/");
   },
 
   after: browser => {
     browser.end();
   },
 
-  "should be on google search page": function(browser) {
-    browser.waitForElementVisible("body", 1000).assert.title("Google");
-  },
-
-  "should search for Cheese!": function(browser) {
-    browser
-      .waitForElementVisible(".gLFyf.gsfi", 1000)
-      .setValue("input[type=text]", "Cheese!")
-      .waitForElementVisible("input[name=btnK]", 1000)
-      .click("input[name=btnK]");
-  },
-
-  'the page title should start with "Cheese!': function(browser) {
-    browser
-      .waitForElementVisible("#resultStats", 1000)
-      .pause(1000)
-      .getTitle(title => {
-        const words = title.split(" ");
-        browser.assert.strictEqual(words[0], "Cheese!");
-      });
+  "should be on Sandbox": function(browser) {
+    browser.assert.title("Sandbox");
+    browser.getText("css selector", "h1", function(text) {
+      browser.assert.strictEqual(text.value, "Sandbox");
+    });
   }
 };
